ddc1dce2d53a7ff2f0f90218bcdc40d69b1b7ba2
ArmanAfridiRahman <armanafridirahman@gmail.com>
2023-05-27 22:20:14 +0600

    Initializing the base project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # This is a showcase of my RAW PHP knowledge 
+
+Day 1: 10:19pm: Created the base of my raw php website.
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'raw-php-website/raw-php-website',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f5f3abe76ce277df9b68f168a695e110b940b7c8',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'raw-php-website/raw-php-website' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f5f3abe76ce277df9b68f168a695e110b940b7c8',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
