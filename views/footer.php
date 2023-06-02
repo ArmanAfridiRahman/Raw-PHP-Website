7a2730180d8c878084327e2c40a139351f6280cb
ArmanAfridiRahman <armanafridirahman@gmail.com>
2023-06-02 14:30:09 +0600

    Added static header and footer

---
+++ views/footer.php
@@ -0,0 +1,49 @@
+<div class="modal fade bg-dark bg-opacity-25" id="profileModal">
+    <div class="modal-dialog modal-md p-4 ">
+        <div class="modal-content bg-dark shadow border-0 ">
+            <div class="modal-body border-0 my-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="card card-body border-0 bg-transparent p-0 m-0">
+                            <h4 class="text-warning text-center mb-5">Login Account</h4>
+                            <p class="mx-auto text-center text-white w-75 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus facilis laborum, modi molestiae mollitia natus porro quis rerum voluptates. Aliquid!</p>
+                            <div class="card card-body bg-transparent border-0">
+                                <div class="row mb-3 mx-3">
+                                    <input type="text" placeholder="Username" class="input-focus form-control-lg border-warning rounded-0 mt-2" maxlength="30">
+                                </div>
+                                <div class="row mb-3 mx-3">
+                                    <input type="password" placeholder="Password" class="input-focus form-control-lg border-warning rounded-0 mt-2" maxlength="15">
+                                </div>
+                                <div class="row my-3 mx-2">
+                                    <div class="col m-0 p-0 ms-2">
+                                        <label class="text-white float-start"><input type="checkbox" name="rememberMe"> Keep me signed in </label>
+                                    </div>
+                                    <div class="col">
+                                        <a href="" class="text-warning text-decoration-none float-end">Forgot password?</a>
+                                    </div>
+                                </div>
+
+                                <div class="row mt-3 mx-2">
+                                    <button type="submit" class="shadow btn btn-outline-warning border-3 rounded-5 w-50 mx-auto py-2 fs-5 fw-bolder text-uppercase">login</button>
+                                </div>
+                                <div class="row my-3 mx-2">
+                                    <div class="col text-center my-2 grow-3">
+                                        <a href="" class="text-warning text-decoration-none ">Already a member?</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer border-0">
+                Will Edit it later
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="assets/js/bootstrap.bundle.js"></script>
+</body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/all.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header class="">
+        <section class="">
+            <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+                <div class="container px-5 py-2 px-lg-2">
+                    <a href="" class="navbar-brand">
+                        <i class="fa fa-2x text-bg-warning"> Bran</i>
+                        <i class="fa fa-2x fa-space-shuttle"></i>
+                    </a>
+                    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#menu">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse my-4 my-lg-0" id="menu">
+                        <ul class="nav navbar-nav ms-auto hover-glow">
+                            <li class=""><a href="" class="rounded-3 px-3 nav-link text-uppercase" data-bs-toggle="tab">home</a></li>
+                            <li class=""><a href="" class="rounded-3 px-3 nav-link text-uppercase" data-bs-toggle="tab">about</a></li>
+                            <li class=""><a href="" class="rounded-3 px-3 nav-link text-uppercase" data-bs-toggle="tab  ">contact</a></li>
+                            <li class="dropdown dropdown-hover">
+                                <a href="" class="dropdown-toggle nav-link text-uppercase rounded-3 px-3 ">Celestial Bodies</a>
+                                <ul class="dropdown-menu dropdown-menu-dark py-2 w-50 ms-2 ms-lg-0 ">
+                                    <li class="mx-2 mx-lg-0"><a href="" class="dropdown-item rounded-3 grow-2 w-90 ms-2 px-3">Galaxy</a></li>
+                                    <li class="mx-2 mx-lg-0"><a href="" class="dropdown-item rounded-3 grow-2 w-90 ms-2 px-3">Solar System</a></li>
+                                    <li class="mx-2 mx-lg-0"><a href="" class="dropdown-item rounded-3 grow-2 w-90 ms-2 px-3">Sun</a></li>
+                                    <li class="mx-2 mx-lg-0"><a href="" class="dropdown-item rounded-3 grow-2 w-90 ms-2 px-3">Moon</a></li>
+                                    <li class="mx-2 mx-lg-0"><a href="" class="dropdown-item rounded-3 grow-2 w-90 ms-2 px-3">Comet</a></li>
+                                    <li class="mx-2 mx-lg-0"><a href="" class="dropdown-item rounded-3 grow-2 w-90 ms-2 px-3">Planets</a></li>
+                                </ul>
+                            </li>
+                            <li class=""><a href="" class="nav-link text-uppercase rounded-3 px-3 ">Nasa</a></li>
+                            <li class=""><a href="" class="nav-link text-uppercase rounded-3 px-3 ">SpaceX</a></li>
+                        </ul>
+                        <ul class="navbar-nav ms-lg-5 mt-4 mt-lg-0">
+                            <li class=""><a href="#profileModal" data-bs-toggle="modal" class="nav-link text-uppercase text-bg-warning rounded-3 px-3 grow-3">Profile</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </nav>
+        </section>
+    </header>
