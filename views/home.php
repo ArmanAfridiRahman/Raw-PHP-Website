96c1ed2511bcd4faa25b556c22d1f6eee0e9d5fb
ArmanAfridiRahman <armanafridirahman@gmail.com>
2023-06-09 15:37:48 +0600

    updated home.php

--- views/home.php
+++ views/home.php
@@ -1,31 +1,27 @@
 <?php include "header.php"?>
 
     <div class="carousel slide carousel-fade" data-mbd-ride="carousel" id="carouselVideo">
-<!--        <div class="carousel-indicators">-->
-<!--            <button class="active" type="button" data-mbd-target="#carouselVideo" data-mbd-slide-to="0" aria-current="true" aria-label="Slide 1"></button>-->
-<!--            <button type="button" data-mbd-target="#carouselVideo" data-mbd-slide-to="1" aria-label="Slide 2"></button>-->
-<!--            <button type="button" data-mbd-target="#carouselVideo" data-mbd-slide-to="2" aria-label="Slide 3"></button>-->
-<!--        </div>-->
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <video autoplay loop muted class="img-fluid w-100">
                     <source class="" src="assets/videos/animation.mp4" type="video/mp4">
                 </video>
+
                 <div class="carousel-caption d-none d-lg-flex flex-column justify-content-center h-100">
                     <h1 class="grow-2 fw-bold text-bg-video-one display-3 mb-5 ">“To the mind that is still, <br/>the whole universe surrenders.“<br/><span class="fs-5 fw-bold pt-0 text-white">- Lao Tzu</span></h1>
                     <p class="grow-2 text-white fs-5 d-none d-xl-block mb-4">Our universe is 13.7 billion years old. All the history originated from a birth of a<br/> star. It is a blessing that we are part of this evolution.</p>
                     <a href="#profileModal" class="text-decoration-none" data-bs-toggle="modal">
                         <button class="btn btn-outline-warning w-25 mx-auto border-3 grow-3  text-white fw-bold"> Explore <span class="text-white fa fa-space-shuttle"></span></button>
                     </a>
-
                 </div>
+
                 <div class="carousel-caption d-none d-sm-flex d-lg-none flex-column justify-content-center h-100">
                     <h1 class="grow-2 fw-bold text-bg-video-one display-5 text-start"><span class="text-white">“ </span>To the mind <br/>that is still, the whole <br/>universe surrenders.<span class="text-white"> “</span><br/><span class=" fw-bold pt-0 text-white fs-6">- Lao Tzu</span></h1>
                     <a href="#profileModal" class="text-decoration-none" data-bs-toggle="modal">
                         <button class="btn btn-outline-warning me-0 mt-3 mx-auto border-2 grow-3  text-white fw-bold">Explore <span class="text-white fa fa-space-shuttle"></span></button>
                     </a>
-
                 </div>
+
                 <div class="carousel-caption d-flex d-sm-none flex-column justify-content-center h-100">
                     <h1 class="grow-2 fw-bold text-bg-video-one text-start"><span class="text-white">“ </span>To the mind <br/>that is still, the whole <br/>universe surrenders.<span class="text-white"> “</span><br/><span class=" fw-bold pt-0 text-white fs-6">- Lao Tzu</span></h1>
                     <a href="#profileModal" class="text-decoration-none" data-bs-toggle="modal">
@@ -87,7 +83,47 @@
                     </div>
                 </div>
                 <div class="col-lg-4">
+                    <div id="slider" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000"> <!-- delete data-bs-ride="carousel" to stop auto moving carousel -->
+                        <div class="carousel-indicators">
+                            <button data-bs-target="#slider" data-bs-slide-to="0" class="active"> </button>
+                            <button data-bs-target="#slider" data-bs-slide-to="1"> </button>
+                            <button data-bs-target="#slider" data-bs-slide-to="2"> </button>
+                            <button data-bs-target="#slider" data-bs-slide-to="3"> </button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active ">
+                                <img src="assets/img/1.jpg" alt="" class="w-100" height="265"/>
+<!--                                <div class="carousel-caption my-caption">-->
+<!--                                    <h1>This is caption one</h1>-->
+<!--                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse iste labore neque officiis, recusandae ullam! Aperiam atque culpa dolor expedita fugiat fugit, impedit ipsum nobis quisquam ratione temporibus vel veniam.</p>-->
+<!--                                    <a href="" class="btn btn-success">Read More ..</a>-->
+<!--                                </div>-->
+                            </div>
+                            <div class="carousel-item">
+                                <img src="assets/img/glaxy.jpg" alt="" class="w-100" height="265"/>
 
+                            </div>
+                            <div class="carousel-item">
+                                <img src="assets/img/1.jpg" alt="" class="w-100" height="265"/>
+
+                            </div>
+
+                            <div class="carousel-item">
+                                <img src="assets/img/glaxy.jpg" alt="" class="w-100" height="265"/>
+
+                            </div>
+
+
+
+                        </div>
+
+                        <a href="#slider" class="carousel-control-prev" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon"></span>
+                        </a>
+                        <a href="#slider" class="carousel-control-next" data-bs-slide="next">
+                            <span class="carousel-control-next-icon"></span>
+                        </a>
+                    </div>
                 </div>
                 <div class="col-lg-4">
                     <div class="card bg-transparent">
@@ -133,88 +169,13 @@
                 </div>
             </div>
         </div>
-
-
-
-
-
-<!--        <div class="container">-->
-<!--            <div class="row">-->
-<!--                <div class="col-lg-12">-->
-<!--                    <div class="container mb-4">-->
-<!--                        <div class="row">-->
-<!--                            <div class="col-3">-->
-<!--                                <img src="assets/img/home/about/nasa.png" alt="" class="w-100"">-->
-<!--                            </div>-->
-<!--                            <div class="col-6">-->
-<!--                                <div class="accordion" id="myAccordian">-->
-<!--                                    <div class="card rounded-0 border-0">-->
-<!--                                        <div class="card-header rounded-0 border-0 bg-transparent">-->
-<!--                                            <button type="button" data-bs-toggle="collapse" data-bs-target="#contentOne" class="border-0 bg-transparent text-white btn btn-light w-100">This is content one</button>-->
-<!--                                        </div>-->
-<!--                                        <div class="show card-body p-0 collapse bg-dark" id="contentOne" data-bs-parent="#myAccordian">-->
-<!--                                            <p class="text-white p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus facere modi officiis. Iste, reiciendis, repellendus?</p>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-<!--                                    <div class="card rounded-0 border-0">-->
-<!--                                        <div class="card-header rounded-0 border-0 bg-dark">-->
-<!--                                            <button type="button" data-bs-toggle="collapse" data-bs-target="#contentTwo" class="border-0 bg-dark text-white btn btn-light w-100">This is content one</button>-->
-<!--                                        </div>-->
-<!--                                        <div class="card-body p-0 collapse bg-dark" id="contentTwo" data-bs-parent="#myAccordian">-->
-<!--                                            <p class="text-white p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus facere modi officiis. Iste, reiciendis, repellendus?</p>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-<!--                                    <div class="card rounded-0 border-0">-->
-<!--                                        <div class="card-header rounded-0 border-0 bg-dark">-->
-<!--                                            <button type="button" data-bs-toggle="collapse" data-bs-target="#contentThree" class="border-0 bg-dark text-white btn btn-light w-100">This is content one</button>-->
-<!--                                        </div>-->
-<!--                                        <div class="card-body p-0 collapse bg-dark" id="contentThree" data-bs-parent="#myAccordian">-->
-<!--                                            <p class="text-white p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus facere modi officiis. Iste, reiciendis, repellendus?</p>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-<!--                                    <div class="card rounded-0 border-0">-->
-<!--                                        <div class=" card-header rounded-0 border-0 bg-dark">-->
-<!--                                            <button type="button" data-bs-toggle="collapse" data-bs-target="#contentFour" class="border-0 bg-dark text-white btn btn-light w-100">This is content one</button>-->
-<!--                                        </div>-->
-<!--                                        <div class="show card-body p-0 collapse bg-dark" id="contentFour" data-bs-parent="#myAccordian">-->
-<!--                                            <p class="text-white p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus facere modi officiis. Iste, reiciendis, repellendus?</p>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-<!--                                    <div class="card rounded-0 border-0">-->
-<!--                                        <div class="card-header rounded-0 border-0 bg-dark">-->
-<!--                                            <button type="button" data-bs-toggle="collapse" data-bs-target="#contentFive" class="border-0 bg-dark text-white btn btn-light w-100">This is content one</button>-->
-<!--                                        </div>-->
-<!--                                        <div class="card-body p-0 collapse bg-dark" id="contentFive" data-bs-parent="#myAccordian">-->
-<!--                                            <p class="text-white p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus facere modi officiis. Iste, reiciendis, repellendus?</p>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-<!--                                    <div class="card rounded-0 border-0">-->
-<!--                                        <div class="card-header rounded-0 border-0 bg-dark">-->
-<!--                                            <button type="button" data-bs-toggle="collapse" data-bs-target="#contentSix" class="border-0 bg-dark text-white btn btn-light w-100">This is content one</button>-->
-<!--                                        </div>-->
-<!--                                        <div class="card-body p-0 collapse bg-dark" id="contentSix" data-bs-parent="#myAccordian">-->
-<!--                                            <p class="text-white p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus facere modi officiis. Iste, reiciendis, repellendus?</p>-->
-<!--                                        </div>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                            </div>-->
-<!--                            <div class="col-3">-->
-<!--                                <img src="assets/img/home/about/SpaceX.png" alt="" class="w-100">-->
-<!--                            </div>-->
-<!---->
-<!--                        </div>-->
-<!--                    </div>-->
-<!---->
-<!--                </div>-->
-<!--            </div>-->
-<!--        </div>-->
     </section>
 
     <section id="contact" class="py-5 bg-warning">
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <h1 class="text-center">Contact</h1>
+
                 </div>
             </div>
         </div>
